6c23cfc586356446a9f0d1eac9aa4e917148475c
PRIYANSHU <tpriyanshu775@gmail.com>
2021-12-31 12:02:30 +0530

    Add project files.

---
+++ Caretaker/Caretaker/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Caretaker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Login", "LoginSuccessful", "OK");
+        }
+    }
+}
